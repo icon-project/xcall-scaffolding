5843b9e6c50d212b608afb67361ec0e16a29912a
FidelVe <43641727+FidelVe@users.noreply.github.com>
2023-08-15 11:35:38 -0400

    working on e2e tests

--- scripts/e2e/e2e-helloWorld.js
+++ scripts/e2e/e2e-helloWorld.js
@@ -1,34 +1,93 @@
-const { JVM_XCALL_ADDRESS, EVM_NETWORK_LABEL } = require("../config");
+const {
+  JVM_XCALL_ADDRESS,
+  EVM_NETWORK_LABEL,
+  JVM_NETWORK_LABEL
+} = require("../config");
 const { getTxResult, isValidHexAddress } = require("../utils");
-const { initializeJvmContract, getBtpAddress } = require("./utils");
+const {
+  initializeJvmContract,
+  getBtpAddress,
+  invokeJvmDAppMethod,
+  // decodeMessage,
+  encodeMessage,
+  filterCallMessageSentEventJvm,
+  parseCallMessageSentEventJvm,
+  filterCallMessageEventEvm
+} = require("./utils");
 
 async function helloWorldE2E(deployments) {
-  // get BTP address of EVM dApp
+  // get BTP addresses
   const evmDappBtpAddress = getBtpAddress(
     EVM_NETWORK_LABEL,
     deployments.HelloWorld.evm
   );
 
+  const jvmDappBtpAddress = getBtpAddress(
+    JVM_NETWORK_LABEL,
+    deployments.HelloWorld.jvm
+  );
+
   // initialize JVM contract and test results
-  const response_01 = await initializeJvmContract(deployments.HelloWorld.jvm, {
+  const request1 = await initializeJvmContract(deployments.HelloWorld.jvm, {
     _sourceXCallContract: JVM_XCALL_ADDRESS,
     _destinationBtpAddress: evmDappBtpAddress
   });
   console.log(
-    `> Testing valid tx hash for invocation of 'initialize' method on JVM contract: ${
-      isValidHexAddress(response_01, "0x", 66) == true ? "SUCCESS" : "FAILURE"
+    `> Test: invoking 'initialize' method on JVM contract: ${
+      isValidHexAddress(request1, "0x", 66) == true ? "SUCCESS" : "FAILURE"
     }`
   );
-  const txResult_01 = await getTxResult(response_01);
+  const txResult1 = await getTxResult(request1);
   console.log(
-    `> Testing valid invocation of 'initialize' method on JVM contract: ${
-      txResult_01.status == 1 ? "SUCCESS" : "FAILURE"
+    `> Test: validate tx for invoking 'initialize' method on JVM contract: ${
+      txResult1.status == 1 ? "SUCCESS" : "FAILURE"
     }`
   );
 
   // send a message to EVM dApp and test results
+  const msg = encodeMessage("Hello World!");
+  const rollbackData = encodeMessage("Hello World! Rollback");
+  const request2 = await invokeJvmDAppMethod(
+    deployments.HelloWorld.jvm,
+    "sendMessage",
+    {
+      payload: msg,
+      rollback: rollbackData
+    }
+  );
+  console.log(
+    `> Test: invoking 'sendMessage' method on JVM contract: ${
+      isValidHexAddress(request2, "0x", 66) == true ? "SUCCESS" : "FAILURE"
+    }`
+  );
+  const txResult2 = await getTxResult(request2);
+  console.log(
+    `> Test: validate tx for invoking 'sendMessage' method on JVM contract: ${
+      txResult2.status == 1 ? "SUCCESS" : "FAILURE"
+    }`
+  );
+
   // catch CallMessageSent event from JVM contract and test result
+  const eventlog1 = filterCallMessageSentEventJvm(txResult2.eventLogs);
+  console.log(
+    `> Test: filtering 'CallMessageSent' event on JVM contract: ${
+      eventlog1.length > 0 ? "SUCCESS" : "FAILURE"
+    }`
+  );
+  const parsedEventlog1 = parseCallMessageSentEventJvm(eventlog1);
+
   // catch CallMessage event from EVM dApp and test result
+  const eventlog2 = filterCallMessageEventEvm(
+    jvmDappBtpAddress,
+    deployments.HelloWorld.evm,
+    parsedEventlog1._sn
+  );
+  console.log(
+    `> Test: filtering 'CallMessage' event on EVM contract: ${
+      eventlog2 != null ? "SUCCESS" : "FAILURE"
+    }`
+  );
+  console.log(eventlog2);
   // invoke executeCall on EVM chain and test result
   // catch CallExecuted event from EVM dApp and test result
 }
--- scripts/e2e/utils.js
+++ scripts/e2e/utils.js
@@ -10,8 +10,9 @@ const {
   JVM_NID
 } = require("../config");
 const { isValidHexAddress } = require("../utils");
-
 const { Web3 } = require("web3");
+const { ethers } = require("ethers");
+const { xcallAbi } = require("./xcallAbi");
 const web3Utils = Web3.utils;
 
 const IconService = require("icon-sdk-js");
@@ -27,6 +28,43 @@ const { CallTransactionBuilder, CallBuilder } = IconBuilder;
 const HTTP_PROVIDER = new HttpProvider(JVM_RPC);
 const JVM_SERVICE = new IconService.default(HTTP_PROVIDER);
 const JVM_WALLET = IconWallet.loadPrivateKey(JVM_PRIVATE_KEY);
+const EVM_PROVIDER = new ethers.providers.JsonRpcProvider(EVM_RPC);
+const EVM_SIGNER = new ethers.Wallet(EVM_PRIVATE_KEY, EVM_PROVIDER);
+
+function getContractObjectEvm(abi, address) {
+  try {
+    const contract = new ethers.Contract(address, abi, EVM_SIGNER);
+    return contract;
+  } catch (e) {
+    console.log("Error getting contract object: ", e);
+    throw new Error("Error getting contract object");
+  }
+}
+
+function getXcallContractObject() {
+  try {
+    return getContractObjectEvm(xcallAbi, JVM_XCALL_ADDRESS);
+  } catch (e) {
+    console.log("Error getting xcall contract object: ", e);
+    throw new Error("Error getting xcall contract object");
+  }
+}
+
+function filterEventEvm(contract, method, ...params) {
+  const filter = contract.filters[method](...params);
+  return filter;
+}
+
+function filterCallMessageEventEvm(btpAddressSource, evmDappAddress, sn) {
+  const xCallContract = getXcallContractObject();
+  return filterEventEvm(
+    xCallContract,
+    "CallMessage",
+    btpAddressSource,
+    evmDappAddress,
+    sn
+  );
+}
 
 async function getXcallJvmFee(network, useRollback = true) {
   try {
@@ -85,6 +123,15 @@ async function initializeJvmContract(dappContract, params) {
   return await invokeJvmContractMethod("initialize", dappContract, params, fee);
 }
 
+async function invokeJvmDAppMethod(
+  dappContract,
+  method,
+  params,
+  rollback = true
+) {
+  const fee = await getXcallJvmFee(EVM_NETWORK_LABEL, rollback);
+  return await invokeJvmContractMethod(method, dappContract, params, fee);
+}
 function getBtpAddress(label, address) {
   return `btp://${label}/${address}`;
 }
@@ -97,8 +144,39 @@ function encodeMessage(msg) {
   return web3Utils.fromUtf8(msg);
 }
 
+function filterCallMessageSentEventJvm(eventlogs) {
+  return filterEventJvm(eventlogs, "CallMessageSent(Address,str,int,int)");
+}
+
+function filterEventJvm(eventlogs, sig, address = JVM_XCALL_ADDRESS) {
+  return eventlogs.filter(event => {
+    return (
+      event.indexed &&
+      event.indexed[0] === sig &&
+      (!address || address === event.scoreAddress)
+    );
+  });
+}
+
+function parseCallMessageSentEventJvm(event) {
+  const indexed = event[0].indexed || [];
+  const data = event[0].data || [];
+  return {
+    _from: indexed[1],
+    _to: indexed[2],
+    _sn: indexed[3],
+    _nsn: data[0]
+  };
+}
+
 module.exports = {
   initializeJvmContract,
   getBtpAddress,
-  isValidHexAddress
+  isValidHexAddress,
+  invokeJvmDAppMethod,
+  decodeMessage,
+  encodeMessage,
+  filterCallMessageSentEventJvm,
+  parseCallMessageSentEventJvm,
+  filterCallMessageEventEvm
 };
