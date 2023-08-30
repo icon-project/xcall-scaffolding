7dff60739b52474814359ad47f454203237b261b
FidelVe <43641727+FidelVe@users.noreply.github.com>
2023-08-30 09:19:39 -0400

    added e2e tests for votingDapp

--- scripts/e2e/e2e-main.js
+++ scripts/e2e/e2e-main.js
@@ -1,13 +1,16 @@
 const { utils } = require("../utils");
 const { getDeployments } = utils;
 const { helloWorldE2E } = require("./e2e-helloWorld");
+const { votingDappE2E } = require("./e2e-votingDapp");
 
 async function main() {
   const deployments = await getDeployments();
 
   console.log("\n>>>>>> Running E2E tests");
   console.log(">>>> Running helloWorldE2E");
-  await helloWorldE2E(deployments);
+  // await helloWorldE2E(deployments);
+  console.log(">>>> Running helloWorldE2E");
+  await votingDappE2E(deployments);
 }
 
 main();
